114b40429558a2fb5ac037ea93ed03e17a1e8a1a
itc-n24004 <n24004@std.it-college.ac.jp>
2024-07-24 08:24:06 +0000

    課題

---
+++ CHAPTER05_Q5_2_2.py
@@ -0,0 +1,4 @@
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+a = [x if x%2==0 else None for x in l]
+print(a)
+print("答え4")
---
+++ CHAPTER05_Q5_2_3.py
@@ -0,0 +1,3 @@
+a = [x * y for x in range(3) for y in range(x+1)]
+print(a)
+print("答え3")
