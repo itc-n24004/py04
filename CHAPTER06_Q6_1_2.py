f42f4852eb895febb52d6b77ac3b20b511749e9b
itc-n24004 <n24004@std.it-college.ac.jp>
2024-07-31 10:36:20 +0000

    syuusei

--- CHAPTER06_Q6_1_2.py
+++ CHAPTER06_Q6_1_2.py
@@ -1,1 +1,22 @@
 class Person:
+    def __init__ (self, 
+            name = '',
+            nationality = '',
+            birth = '',
+            address = ''):
+        self.name = name
+        self.nationality = nationality
+        self.birth = birth
+        self.address = address
+
+
+    def show_attributes(self):
+        print("名前:", self.name)
+        print("国籍:", self.nationality)
+        print("生年月日:", self.birth)
+        print("出身地:", self.address)
+
+
+AuthorofAttackonTitan = Person('諫山創', '日本', '１９８９年８月２９日', '大分県日田市大山町')
+
+AuthorofAttackonTitan.show_attributes()
