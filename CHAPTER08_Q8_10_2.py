db1a5f7721a854b0341c61e22d2df08240631e2f
itc-n24004 <n24004@std.it-college.ac.jp>
2024-08-15 08:29:30 +0000

    syuusei

--- CHAPTER08_Q8_10_2.py
+++ CHAPTER08_Q8_10_2.py
@@ -3,7 +3,7 @@ import logging
 def devide(x1, x2):
     return x1 / x2
 
-logging.basicConfig(filename='testlog.log', level=logging.WARNING, format='%(levelname)s:%(asctime)s:%(messags)s')
+logging.basicConfig(filename='testlog.log', level=logging.WARNING, format='%(levelname)s:%(asctime)s:%(message)s')
 
 try:
     ret = devide(10, 0)
